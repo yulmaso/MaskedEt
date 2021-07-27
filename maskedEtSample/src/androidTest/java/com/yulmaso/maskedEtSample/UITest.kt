3f1e1167c5ef63fc247edb742a7bad58d1598998
yulmaso <plotnikovaa@ds24.ru>
2021-07-27 17:51:05 +0500

    Added tests

--- maskedEtSample/src/androidTest/java/com/yulmaso/maskedEtSample/UITest.kt
+++ maskedEtSample/src/androidTest/java/com/yulmaso/maskedEtSample/UITest.kt
@@ -1,8 +1,8 @@
 package com.yulmaso.maskedEtSample
 
+import android.view.KeyEvent
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -24,38 +24,51 @@ class UITest {
     val mActivityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun testCorrectInput() {
+    fun testCorrectInput1() {
         val et = onView(withId(R.id.et))
 
-        et.perform(click())
-        Thread.sleep(DELAY)
-        et.perform(typeText("1"))
-        Thread.sleep(DELAY)
-        et.perform(typeText("1"))
-        Thread.sleep(DELAY)
-        et.perform(typeText("1"))
-        Thread.sleep(DELAY)
-        et.perform(typeText("1"))
-        Thread.sleep(DELAY)
-        et.perform(typeText("1"))
-        Thread.sleep(DELAY)
-        et.perform(typeText("1"))
-        Thread.sleep(DELAY)
-        et.perform(typeText("1"))
-        Thread.sleep(DELAY)
-        et.perform(typeText("1"))
-        Thread.sleep(DELAY)
-        et.perform(typeText("1"))
-        Thread.sleep(DELAY)
-        et.perform(typeText("1"))
-        Thread.sleep(DELAY)
-        et.perform(typeText("1"))
-        Thread.sleep(DELAY)
-        et.perform(typeText("1"))
-        Thread.sleep(DELAY)
-        et.perform(typeText("1"))
+        et.perform(click(), typeText("1111111111"))
         Thread.sleep(DELAY)
 
-        onView(withId(R.id.et)).check(matches(withText("+7 (111) 111-11-11")))
+        et.check(matches(withText("+7 (111) 111-11-11")))
+
+        onView(withId(R.id.raw_text_btn)).perform(click())
+        Thread.sleep(DELAY)
+        onView(withId(R.id.tv)).check(matches(withText("1111111111")))
+    }
+
+    @Test
+    fun testCorrectInput5() {
+        val et = onView(withId(R.id.et))
+
+        et.perform(click(), typeText("5555555555"))
+        Thread.sleep(DELAY)
+
+        et.check(matches(withText("+7 (555) 555-55-55")))
+
+        onView(withId(R.id.raw_text_btn)).perform(click())
+        Thread.sleep(DELAY)
+        onView(withId(R.id.tv)).check(matches(withText("5555555555")))
+    }
+
+    @Test
+    fun testInputAndDelete() {
+        val et = onView(withId(R.id.et))
+
+        et.perform(
+            click(),
+            typeText("55555"),
+            pressKey(KeyEvent.KEYCODE_DEL),
+            pressKey(KeyEvent.KEYCODE_DEL),
+            pressKey(KeyEvent.KEYCODE_DEL),
+            typeText("55778890")
+        )
+        Thread.sleep(DELAY)
+
+        et.check(matches(withText("+7 (555) 577-88-90")))
+
+        onView(withId(R.id.raw_text_btn)).perform(click())
+        Thread.sleep(DELAY)
+        onView(withId(R.id.tv)).check(matches(withText("5555778890")))
     }
 }
